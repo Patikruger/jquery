44267c377dd25119f86a4f555b3c7c6a3d701c8e
Patricia Kruger <patypietrak5@gmail.com>
2024-04-10 20:59:31 -0300

    jquery

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    console.log(document.querySelector('header button'))
+    console.log($('#botao cancelar'))
+
+    document.querySelector('Header button').addEventListener('click', function() {
+
+    })
+
+    $h('header button').click(function() {
+        alert("Expandir Formulario")
+    })
+});
+
+
